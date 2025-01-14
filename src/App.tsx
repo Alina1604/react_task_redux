cd454eacf855c417cb21d4ccd39be0d676aa5cc8
Artem Kukuruzza <artem.kuka@gmail.com>
2025-01-14 18:52:40 +0300

    code review

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,15 @@ import LoginPage from './components/pages/LoginPage';
 import HomePage from './components/pages/HomePage';
 import ProtectedPage from './components/pages/ProtectedPage';
 import PaginatedPage from './components/pages/PaginatedPage';
+
 import './index.css';
 
+// все, что связано с инициализацией глобального стейта обязательно выносить в файлы, 
+// связанные с этим глобальным стейтом,
+// то есть `store` следуюет вынести в store.tsx
+
+// а `rootReducer` следует вынести в store/reducers/index.tsx 
+// комментарии для файла c редьюсерами внутри файла reducer.tsx
 const rootReducer = combineReducers({
     auth: loginReducer,
     pagination: paginationReducer,
@@ -34,11 +41,17 @@ const App: React.FC = () => {
     );
 };
 
+
+// ProtectedRoute и ProtectedRouteProps следует вынести в отдельный компонент, поскольку это самостоятельная логика.
+// Внутри компонентов должна быть только та часть кода, которая нужна именно этому компоненту.
+// В данном случае ProtectedRoute выполняет самостоятельную задачу.
+
 interface ProtectedRouteProps {
     component: React.FC;
 }
 
 const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ component: Component }) => {
+    // от any нужно по-возможности избавляться
     const isAuthenticated = useSelector((state: any) => state.auth.isAuthenticated);
     return isAuthenticated ? <Component /> : <Navigate to="/login" />;
 };
